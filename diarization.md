e795abb02b173386481a23f20ee974f0859b717f
Tricia Amheiser <tricia@taarts.net>
2022-11-03 16:33:16 -0400

    Added API to create a new game

---
+++ diarization.md
@@ -0,0 +1,19 @@
+board: [
+[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+],
+id: null,
+state: null,
+mines: 9,
+
+created a board (starting with 3 rows to begin)
+
+create a handleClickCell (ultimately to activate a cell to reveal a "square")
+
+hook up button to create a new game
--- src/App.tsx
+++ src/App.tsx
@@ -1,85 +1,67 @@
 import React, { useState, useEffect } from 'react'
 
-// export function App() {
-type Cell = '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | 'F' | '*' | '-' | ' '
-
-type Row = [Cell, Cell, Cell, Cell, Cell, Cell, Cell, Cell]
-
-type board = [Row, Row, Row, Row, Row, Row, Row, Row]
-
-type Game = {
-board: board
-id: null | number
-state: null | 'playing' | 'won' | 'lost' | 'new' 
-mines: 9
-}
-
-// const [game, setGame] = useState<Board>({
-//   id: null,
-//   // winner: null,
-// })
-
 export function App() {
-const [game, setGame] = useState<Game>({
-board: [
-[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-],
-id: null,
-state: null,
-mines: 9,
-})
+  const [game, setGame] = useState({
+    board: [
+      [' ', '1', 'f', ' '],
+      [' ', '2', ' ', ' *'],
+      [' ', '- ', ' ', 'f'],
+      [' ', '- ', ' ', ' '],
+      // [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      // [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      // [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      // [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      // [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    ],
+    id: null,
+    state: null,
+    mines: 9,
+  })
 
-
-
-
-
-// const body = { row, column }
-
-
-
-async function handleClickCell(row: number, column: number){
-    const url = `https://minesweeper-api.herokuapp.com/games/${game.id}`
-}
-
-  async function handleNewGame() {
-    const response = await fetch('https://minesweeper-api.herokuapp.com/games', 
-    {
-      method: 'POST',
-      headers: { 'content-type' : 'application/json' },
-        }
+  function handleClickCell(row: number, col: number) {
+    console.log({ row, col })
+  }
+  async function _newGame() {
+    const response = await fetch(
+      'https://minesweeper-api.herokuapp.com/games',
+      {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+      }
     )
     if (response.ok) {
-      const newGameState = (await response.json()) as Game
+      const newGameState = await response.json()
       setGame(newGameState)
     }
-  
-}
+  }
+  const header = 'Minesweeper'
 
-// game
-const header = game.state ? `${game.state}` : 'Minesweeper'
-return (
-<div>
-<h1>{header} - {game.id} <button onClick={handleNewGame}>New</button></h1>
-{/* <button onClick={() => newGame(0)}>easy</button> */}
-<main>
-  <ul>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-  </ul>
-</main>
-</div>
-)}
+  return (
+    <div>
+      <h1>
+        {header} - {game.id} <button onClick={_newGame}>New Game</button>
+      </h1>
+
+      <main>
+        <ul>
+          <li onClick={() => handleClickCell(0, 0)}>{game.board[0][0]}</li>
+          <li onClick={() => handleClickCell(0, 1)}>{game.board[0][1]}</li>
+          <li onClick={() => handleClickCell(0, 2)}>{game.board[0][2]}</li>
+          <li onClick={() => handleClickCell(0, 3)}>{game.board[0][3]}</li>
+          <li onClick={() => handleClickCell(1, 0)}>{game.board[1][0]}</li>
+          <li onClick={() => handleClickCell(1, 1)}>{game.board[1][1]}</li>
+          <li onClick={() => handleClickCell(1, 2)}>{game.board[1][2]}</li>
+          <li onClick={() => handleClickCell(1, 3)}>{game.board[1][3]}</li>
+          <li onClick={() => handleClickCell(2, 0)}>{game.board[2][0]}</li>
+          <li onClick={() => handleClickCell(2, 1)}>{game.board[2][1]}</li>
+          <li onClick={() => handleClickCell(2, 2)}>{game.board[2][2]}</li>
+          <li onClick={() => handleClickCell(2, 3)}>{game.board[2][3]}</li>
+          <li onClick={() => handleClickCell(3, 0)}>{game.board[3][0]}</li>
+          <li onClick={() => handleClickCell(3, 1)}>{game.board[3][1]}</li>
+          <li onClick={() => handleClickCell(3, 2)}>{game.board[3][2]}</li>
+          <li onClick={() => handleClickCell(3, 3)}>{game.board[3][3]}</li>
+        </ul>
+      </main>
+    </div>
+  )
+}
